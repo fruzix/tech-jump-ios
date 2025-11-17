a878e5c93e6d93930746a6b86a909c9e9681ddd2
Aleksandra Niewińska <aleksandra.niewinska@schibsted.com>
2025-11-17 14:25:14 +0100

    chore: update swiftlint lint rules and lint project

--- TechJumpIOSTests/TechJumpIOSTests.swift
+++ TechJumpIOSTests/TechJumpIOSTests.swift
@@ -5,13 +5,11 @@
 //  Created by Aleksandra Niewińska on 17/11/2025.
 //
 
-import Testing
 @testable import TechJumpIOS
+import Testing
 
 struct TechJumpIOSTests {
-
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
-
 }
